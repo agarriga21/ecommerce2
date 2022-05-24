a228c678acda7d94d2ebd316daa1100cedb7e894
Alex <agarriga2114@gmail.com>
2022-05-24 11:07:43 -0500

    All files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  luv2shopApiUrl: "http://localhost:8080/api",
+  stripePublishableKey: "pk_test_51KgZckE6okuwhGCfjABpYUUcNI0kbbK36WsnseXatx6xdoSKMsF7hO6mSLHDvNocKnQYdeMI9bwbBCXJpiIKMbM3002aJHIZkX"
+};
