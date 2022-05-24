7c5e0bda5f9d0cc5d3909677f73d41c5a52184ac
agarriga21 <78110107+agarriga21@users.noreply.github.com>
2022-05-24 11:20:52 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  luv2shopApiUrl: "http://localhost:8080/api",
+  luv2shopApiUrl: "https://ecom2back.azurewebsites.net/api",
   stripePublishableKey: "pk_test_51KgZckE6okuwhGCfjABpYUUcNI0kbbK36WsnseXatx6xdoSKMsF7hO6mSLHDvNocKnQYdeMI9bwbBCXJpiIKMbM3002aJHIZkX"
 };
